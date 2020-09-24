2d89f7d9a6d7abe60013ee2d7555cc24dd74b1b9
miimss <51400824+miimss@users.noreply.github.com>
2020-09-24 15:24:40 +0300

    added api

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.bookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
@@ -26,6 +28,16 @@ public class BookController {
     public String bookList(Model model) {	
         model.addAttribute("books", brepository.findAll());
         return "booklist";
+    }
+    
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+    	return (List<Book>) brepository.findAll();
+    }
+    
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) { 
+    	return brepository.findById(bookId);
     }
     
     @RequestMapping(value="/add")
